01cd52c1ca38395be10999b9defe6b03c1bae9db
Viktor <vikt-831@bk.ru>
2021-03-25 00:19:02 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+//alert('Hello');
+
+//const result=confirm("Are you here");
+//console.log(result);
+
+//const answer=+prompt("Вам есть18","");
+//console.log(typeof(answer));
+
+/*const answers=[];
+answers[0]=prompt("Как ваше имя","");
+answers[1]=prompt("Как ваша фамилия","");
+answers[2]=prompt("Сколько вам лет","");
+//const answers=[];
+//console.log(typeof(answers));*/
+
+//const category="toys";
+//console.log(`http://someurl.com/${category}/5`);
+let incr=10;
+let decr=10;
+console.log(incr++);
+console.log(decr++);
